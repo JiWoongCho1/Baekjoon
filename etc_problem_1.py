9f7d3d748d070b59facd6a49016af7e87dfbdcfc
Choi Ji Woong <choiji320@naver.com>
2022-02-01 18:56:21 +0900

    first commit

---
+++ etc_problem_1.py
@@ -0,0 +1,12 @@
+# 문제 : https://www.acmicpc.net/problem/1158
+
+n, k = map(int, input().split())
+stack = [i for i in range(1,n+1)]
+answer = []  # 제거된 사람들 넣어줌
+num = 0 # 제거 될 사람의 인덱스
+for i in range(n):
+    num += k-1
+    if num >= len(stack):
+        num = num % len(stack)
+    answer.append(str(stack.pop(num)))
+print("<", ", ".join(answer)[:], ">", sep = '')
