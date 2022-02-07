38adb3cf239f23489b52f1658bc2a4b9c2097d74
JiWoongCho1 <90513931+JiWoongCho1@users.noreply.github.com>
2022-02-07 16:27:06 +0900

    first

---
+++ combinations.py
@@ -0,0 +1,16 @@
+from math import factorial
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+def count_number(n, k):
+    count = 0
+    while n:
+        n //= k
+        count += n
+    return count
+
+five_count = count_number(n,5) - count_number(m,5) -count_number(n-m, 5)
+two_count = count_number(n,2) - count_number(m,2) -count_number(n-m, 2)
+print(min(five_count, two_count))
