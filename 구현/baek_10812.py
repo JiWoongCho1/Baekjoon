1177436fca964dc90e4c06293c9299e70999163e
jiwoong <choiji320@gmail.com>
2023-04-12 13:01:25 +0900

    first commit

---
+++ 구현/baek_10812.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+
+pocket = [i for i in range(1, n+1)]
+
+for _ in range(m):
+    begin, end, mid = map(int, input().split())
+
+    pocket = pocket[:begin-1]+ pocket[mid-1:end] + pocket[begin-1:mid-1] + pocket[end:]
+for i in pocket:
+    print(i, end = ' ')
+
