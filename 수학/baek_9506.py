e935d54087dbc3b329038ca0813fa898517cf03d
jiwoong <choiji320@gmail.com>
2023-04-18 10:37:49 +0900

    first commit

---
+++ 수학/baek_9506.py
@@ -0,0 +1,18 @@
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    answer_list = []
+    for i in range(1, n):
+        if n % i == 0:
+            answer_list.append(i)
+    if sum(answer_list) == n:
+        print('{} ='.format(n), end=' ')
+        for j in answer_list:
+            if j != answer_list[-1]:
+                print('{} +'.format(j), end = ' ')
+            else:
+                print('{}'.format(j))
+
+    else:
+        print('{} is NOT perfect.'.format(n))
