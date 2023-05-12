908c862967ce1506f1d89ccdb01868976d8fef6d
jiwoong <choiji320@gmail.com>
2023-05-12 21:54:00 +0900

    first commit

---
+++ 큐/baek_11866.py
@@ -0,0 +1,17 @@
+from collections import deque
+n, k= map(int, input().split())
+
+
+queue = deque([i for i in range(1, n+1)])
+
+print("<", end = '')
+while queue:
+    for i in range(k-1):
+        queue.append(queue.popleft())
+    print(queue.popleft(), end = '')
+    if queue:
+        print(',', end = ' ')
+
+print(">")
+
+
